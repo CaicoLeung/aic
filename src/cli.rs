28462848426b160e319e89ca96a2ad89c9be4f3b
Caico Leung <lkk1053190902@gmail.com>
2026-05-17 18:53:07 +0800

    style: format clap command attribute

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,11 @@
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
-#[command(name = "aic", version, about = "An AI-powered Rust CLI for generating git commit messages in bulk.\naic[https://github.com/CaicoLeung/aic]")]
+#[command(
+    name = "aic",
+    version,
+    about = "An AI-powered Rust CLI for generating git commit messages in bulk.\naic[https://github.com/CaicoLeung/aic]"
+)]
 pub struct Cli {
     #[command(subcommand)]
     pub command: Option<Commands>,
